7b9241a621c3d365e0ff51480bcd3ba87abf52b4
andrecruz0 <andrebrbug@gmail.com>
2025-06-18 19:42:50 -0300

    feat: restructure project by moving Home component to src/screens; add new Button component; include asset images

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { Button, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
-import Home from "./screens/home";
+import Home from "./src/screens/home";
 
 export default function App() {
 	return (
